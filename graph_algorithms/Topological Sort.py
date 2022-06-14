7cbd1c80335be7bf5c4ef3f72632115aac885129
Juliusz Wasieleski <100780753+puszekjuliuszek@users.noreply.github.com>
2022-06-14 03:27:02 +0200

    Update Topological Sort.py

--- graph_algorithms/Topological Sort.py
+++ graph_algorithms/Topological Sort.py
@@ -1,40 +1,31 @@
-# typical dfs form Cormen, but dfs_visit is iterative not recursion
-# implementation for graph as a matrix wich returs list of parents, because idk what else it sould retun
-
 
 def Dfs(G: list) -> list:
     n = len(G)
     time = 0
     times = [float('inf') for _ in range(n)]
     parents = [None for _ in range(n)]
-
+    sorted_vertex = []
     for i in range(n):
         if  times[i] == float('inf'):
-            Dfs_visit(G, i, time, times, parents)
-
-    return times
-
-
-def Dfs_visit(G: list, s: int, time: int, times: list, parents: list) -> None:
-    n = len(G)
-    stack = [(s,time)]
-    times[s] = True
-    while len(stack) > 0:
-        u,time = stack.pop()
-        times[u] = time
-        for i in range(n):
-            if times[i]>time and G[u][i] != float('inf'):
-                parents[i] = u
-                stack.append((i,time+1))
-
-def Topological_sort(G):
-    n = len(G)
-    Times = Dfs(G)
-    Times = [(Times[i],i) for i in range (n)]
-    Times.sort()
-    sorted_vertex = [Times[i][1] for i in range(n)]
+            Dfs_visit(G, i, time, times, parents,sorted_vertex)
 
     return sorted_vertex
+
+
+def Dfs_visit(G: list, s: int, time: int, times: list, parents: list,sorted_vertex: list) -> None:
+    n = len(G)
+    time += 1
+    times [s] = time
+    for i in range(n):
+        if times[i] == float('inf') and G[s][i] != float('inf'):
+            parents[i] = s
+            Dfs_visit(G,i,time,times,parents,sorted_vertex)
+    sorted_vertex.append(s)
+
+
+
+
+    return sorted_vertex.reverse()
 
 if __name__ == "__main__":
     inf = float('inf')
@@ -48,9 +39,10 @@ if __name__ == "__main__":
          [inf, inf, inf,inf, inf, inf, inf, inf, inf,inf, inf, inf, inf, inf],
          [inf, inf, inf,inf, inf, inf, inf, 1, inf,inf, inf, inf, inf, inf],
          [inf, inf, inf,inf, inf, inf, inf, inf, inf,inf, inf, inf, inf, 1],
-         [inf, inf, inf,inf, inf, inf, inf, inf, inf,1, inf, inf, inf, inf],
+         [inf, inf, inf,inf, inf, inf, inf, inf, inf,1, inf, 1, inf, inf],
          [inf, inf, inf,inf, inf, inf, inf, inf, inf,inf, inf, inf, inf, inf],
          [inf, inf, inf,inf, inf, inf, inf, inf, inf,inf, 1, inf, inf, inf],
          [inf, inf, inf,inf, inf, inf, inf, inf, inf,inf, inf, inf, inf, inf]]
 
-    print(Topological_sort(G))
+    
+    print(Dfs(G))
